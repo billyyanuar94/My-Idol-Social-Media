66b68f2c0e03bd1d960c1d39e975923a9a06f033
billyyanuar94 <billyyanuar94@gmail.com>
2016-11-23 10:06:29 +0700

    Coba2 view

---
+++ assets/view/view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Idol Social Media</title>
+</head>
+<body>
+ 	<h1>Idol name</h1>
+ 	<?php
+ 		include "/../class/facebook.php";
+ 		echo getLikePageMyIdol();
+ 	?>
+</body>
+</html>
